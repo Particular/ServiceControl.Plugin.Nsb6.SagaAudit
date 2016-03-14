55087614396ad2749f743375e07724e8d10dc392
Daniel Marbach <daniel.marbach@particular.net>
2016-03-14 15:21:23 +0100

    Initial version

---
+++ src/ServiceControl.Plugin.Nsb6.SagaAudit/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("ServiceControl.Plugin.Nsb6.SagaAudit")]
+[assembly: AssemblyProduct("ServiceControl.Plugin.Nsb6.SagaAudit")]
+[assembly: InternalsVisibleTo("ServiceControl.Plugin.Nsb6.SagaAudit.Tests")]
