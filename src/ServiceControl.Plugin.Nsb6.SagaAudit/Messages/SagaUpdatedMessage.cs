d3c212e0665fd658f10db7c8a12ea4b8e899bd59
Bob Langley <bob.langley@particular.net>
2016-03-22 21:50:40 -0700

    DataContracts no longer necessary

--- src/ServiceControl.Plugin.Nsb6.SagaAudit/Messages/SagaUpdatedMessage.cs
+++ src/ServiceControl.Plugin.Nsb6.SagaAudit/Messages/SagaUpdatedMessage.cs
@@ -2,10 +2,8 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Runtime.Serialization;
     using NServiceBus;
 
-    [DataContract]
     class SagaUpdatedMessage:IMessage
     {
         public SagaUpdatedMessage()
@@ -13,25 +11,24 @@
             ResultingMessages = new List<SagaChangeOutput>();
         }
 
-        [DataMember]
         public string SagaState { get; set; }
-        [DataMember]
+
         public Guid SagaId { get; set; }
-        [DataMember]
+
         public SagaChangeInitiator Initiator { get; set; }
-        [DataMember]
+
         public List<SagaChangeOutput> ResultingMessages { get; set; }
-        [DataMember]
+
         public string Endpoint { get; set; }
-        [DataMember]
+
         public bool IsNew { get; set; }
-        [DataMember]
+
         public bool IsCompleted { get; set; }
-        [DataMember]
+
         public DateTime StartTime { get; set; }
-        [DataMember]
+
         public DateTime FinishTime { get; set; }
-        [DataMember]
+
         public string SagaType { get; set; }
     }
 }
