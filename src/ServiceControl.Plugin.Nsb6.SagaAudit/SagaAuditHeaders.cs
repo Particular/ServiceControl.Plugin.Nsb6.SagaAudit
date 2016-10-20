159bb1eeacbbd257fce1b8fc67b73cbf82e28e9d
John Simons <john.simons@nservicebus.com>
2016-10-20 12:05:46 +1000

    Missing api doco

--- src/ServiceControl.Plugin.Nsb6.SagaAudit/SagaAuditHeaders.cs
+++ src/ServiceControl.Plugin.Nsb6.SagaAudit/SagaAuditHeaders.cs
@@ -1,7 +1,13 @@
 ﻿namespace ServiceControl.Plugin.SagaAudit
 {
+    /// <summary>
+    /// Headers used by the plugin.
+    /// </summary>
     public class SagaAuditHeaders
     {
+        /// <summary>
+        /// Captures the invoked sagas.
+        /// </summary>
         public const string InvokedSagas = "NServiceBus.InvokedSagas";
     }
 }
